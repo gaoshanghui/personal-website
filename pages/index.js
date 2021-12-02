f174ff65c13bc215bdf830743320bab2da4c764d
Gao Shanghui <mail.gaoshanghui@gmail.com>
2021-12-02 21:27:33 +0900

    Update home page message

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           content="Hello, my name is Gao Shanghui. It is the portfolio to show my recent projects."
         />
       </Head>
-      New portfolio weebsite is coming soon!
+      New portfolio website is coming soon!
       {/* <Header />
       <div className="p-5 space-y-5 mt-[69px]">
         <Card
