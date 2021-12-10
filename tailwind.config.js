4e52c5983e9cd2ab385efc9a5733fbcfa06a4038
Gao Shanghui <mail.gaoshanghui@gmail.com>
2021-12-10 20:50:10 +0900

    Update tailwind to version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  // mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media", // 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        'sans-cn': ['Noto Sans SC', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        "sans-cn": ["Noto Sans SC", ...defaultTheme.fontFamily.sans],
       },
       height: {
-        line: '2px',
+        line: "2px",
       },
       backgroundImage: {
-        'project-cover-to-t':
-          'linear-gradient(14.83deg, rgba(15, 23, 42, 0.51) 2.1%, rgba(15, 23, 42, 0) 67.27%);',
-        'project-cover-to-r':
-          'linear-gradient(90deg, rgba(15, 23, 42, 0.590984) -0.06%, rgba(15, 23, 42, 0) 58.99%);',
+        "project-cover-to-t":
+          "linear-gradient(14.83deg, rgba(15, 23, 42, 0.51) 2.1%, rgba(15, 23, 42, 0) 67.27%);",
+        "project-cover-to-r":
+          "linear-gradient(90deg, rgba(15, 23, 42, 0.590984) -0.06%, rgba(15, 23, 42, 0) 58.99%);",
       },
       inset: {
-        '1/10': '10%',
-        '1/5': '20%',
+        "1/10": "10%",
+        "1/5": "20%",
       },
     },
   },
