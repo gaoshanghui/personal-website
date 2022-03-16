9c5ad04789ab617d0cd62d4a3370418d4e3c078d
Gao Shanghui <mail.gaoshanghui@gmail.com>
2022-03-16 13:30:39 +0900

    Update menu component

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -7,26 +7,30 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ menuIsOpen }) => {
   return (
     <div
-      className={`fixed top-[69px] z-20 w-full bg-white pt-16 pb-20 pl-28 lg:hidden ${
+      className={`fixed top-[68px] z-20 w-full bg-white pt-16 pb-20 pl-[23%] lg:hidden ${
         menuIsOpen ? "block " : "hidden"
       }`}
     >
       <ul className="space-y-10">
         <li>
           <Link href="/">
-            <a className="text-xl font-bold leading-7 text-slate-900">
+            <a className="text-xl font-bold leading-7 tracking-[0.01em] text-slate-900">
               Projects
             </a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a className="text-xl font-bold leading-7 text-slate-900">About</a>
+            <a className="text-xl font-bold leading-7 tracking-[0.01em] text-slate-900">
+              About
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/">
-            <a className="text-xl font-bold leading-7 text-slate-900">Resume</a>
+            <a className="text-xl font-bold leading-7 tracking-[0.01em] text-slate-900">
+              Resume
+            </a>
           </Link>
         </li>
       </ul>
