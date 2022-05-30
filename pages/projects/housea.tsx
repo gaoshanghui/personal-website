275debd933ff89e8e7ab1e236ecc8da45d7249c3
Gao Shanghui <45689448+gaoshanghui@users.noreply.github.com>
2022-05-30 04:23:40 +0000

    Add hero image block

--- pages/projects/housea.tsx
+++ pages/projects/housea.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Header from "../../components/header";
+
+// Images used in this page
+import overviewImage from "../../public/images/housea-overview.png";
+// import overviewBackgroundImage from "../../public/images/housea-overview-bg.png";
 
 const Housea: NextPage = () => {
   return (
@@ -14,6 +19,23 @@ const Housea: NextPage = () => {
         />
       </Head>
       <Header />
+      <div className="mt-[68px] hidden lg:mt-[88px] lg:block lg:p-0">
+        <div className="relative bg-[url('/images/housea-overview-bg.png')]">
+          <h1 className="mx-auto max-w-[1040px] pt-20 pb-4">
+            <span className="text-[28px] font-black leading-8 text-slate-900 block">Housea</span>
+            <span className="body-1 text-slate-900 block">Design a website for finding rental apartments</span>
+          </h1>
+          <div className="relative mx-auto max-w-[1040px]">
+            <Image
+              src={overviewImage}
+              alt="Housea website screenshots"
+              layout="responsive"
+              objectFit="contain"
+              priority
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
