1d4fff89a2a2c3ba3fe75ba12299895795c2f9f9
Gao Shanghui <Shanghui.Gao@hotmail.com>
2023-04-27 15:05:39 +0900

    Remove Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from '@vercel/analytics/react';
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-5XRGEJ4FT5"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-5XRGEJ4FT5');
-        `}
-      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
